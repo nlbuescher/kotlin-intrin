078ba17e26970303083735db61e7e78c70637033
Nicola Buescher <nicolalucasbuescher@gmail.com>
2022-02-15 19:28:34 +0100

    remove unnecessary stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,6 @@ val compileTasks = intrinNames.map { name ->
 		outputs.file(outDir.resolve("$name.o"))
 
 		executable("clang++")
-		args("-v")
 		args("-std=c++20", "-O3", "-m$name", "-c", "-o", outDir.resolve("$name.o"), srcDir.resolve("$name.cpp"))
 	}
 }
@@ -51,7 +50,6 @@ val compileIntrin by tasks.registering(Exec::class) {
 	outputs.file(outDir.resolve("intrin.o"))
 
 	executable("clang++")
-	args("-v")
 	args("-std=c++20", "-O3", "-c", "-o", outDir.resolve("intrin.o"), srcDir.resolve("intrin.cpp"))
 }
 
